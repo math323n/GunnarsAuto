318741bbda39641893ab6d6d714efd334e797e62
sut <math323n@edu.campusvejle.dk>
2020-02-17 14:41:03 +0100

    Add project files.

---
+++ GunnarsAuto/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GunnarsAuto
+{
+    class Car
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string VIN_number { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public string IsUsed { get; set; }
+    }
+}
---
+++ GunnarsAuto/CarRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace GunnarsAuto
+{
+    class CarRepository
+    {
+      
+
+
+        private const string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=GunnarsAutoDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public List<Car> GetAll()
+        {
+            List<Car> cars = new List<Car>();
+            string sql = "SELECT * FROM Cars";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            while(reader.Read())
+            {
+                string make = (string)reader["Make"];
+                string model = (string)reader["Model"];
+                string vin = (string)reader["VIN_number"];
+                string registration = (string)reader["RegistrationsNumber"];
+            }
+            return cars;
+        }
+    }
+}
